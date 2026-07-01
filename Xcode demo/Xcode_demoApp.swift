326dab6e00a459dce9dd22b925bb34f0f1e40422
Adeleke Olasope <adelekeolasope@gmail.com>
2026-07-01 11:31:45 +0100

    Initial Commit

---
+++ Xcode demo/Xcode_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_demoApp.swift
+//  Xcode demo
+//
+//  Created by Adeleke Olasope on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
