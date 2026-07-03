69c526e37ecd4ad8cff63509190f5642bfd4ab77
Adeleke Olasope <adelekeolasope@gmail.com>
2026-07-03 08:49:07 +0100

    Lesson 5

---
+++ SwitchCodePlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting: String = "Hello, playground"
+let myInt: Int = 5
+var myDouble: Double = 0.5
+var myBool: Bool = false
+
+greeting = "I can write Swift"
+print(greeting)
+print(myInt)
+print(myDouble)
+print(myBool)
+
